1ee6a40b4c62895bbfb7da88f0f9a9c70f774046
Amithaviv <amithaviv24@gmail.com>
2022-11-21 02:08:59 +0200

    Add router,home component and fix navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <Navbar className="color-nav" variant="light">
+        <Container id="Container">
+          <img alt="logo" src='car.png' id="logoTitle" ></img>
+          <Nav className="me-auto" id="homenav">
+          <Link to="/Home" type="button" id="HomeNavFont" className="nav-link active">
+            Home
+            </Link>
+            <Link to="/Home" type="button" id="BuyNavFont" className="nav-link active">Buy</Link>
+            <Link to="/Home" type="button" id="AboutNavFont" className="nav-link active">
+            About
+            </Link>
+            <Navbar.Brand id="cartBtn" type="button"><img typeof='button' alt="cart" id="cart" src="cart.png"></img></Navbar.Brand>
+            <Navbar.Brand id="userBtn" type="button"><img typeof='button' alt="user" id="user" src="user.png"></img></Navbar.Brand>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Routes>
+            <Route element={<Home />} path="/Home"></Route>
+    </Routes>
+      </Router>
     </div>
   );
 }
